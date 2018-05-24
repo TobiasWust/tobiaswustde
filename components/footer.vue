e4b4255b51683d2d9db897d9013cf7a203530804
SaftpresseWust <forum@tobiaswust.de>
2018-05-24 10:05:35 +0200

    netlify test

---
+++ components/footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Datenschutz | Impressum
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  background-color: #0d999e;
+}
+</style>
