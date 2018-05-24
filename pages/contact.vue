9d22328baa92761d28251f9bf6adcf6f1114f59e
SaftpresseWust <forum@tobiaswust.de>
2018-05-24 13:21:13 +0200

    trying data-netlify="true"

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Kontakt</h1>
-    <form name="contact" method="POST" netlify>
+    <form name="contact" method="POST" data-netlify="true">
       <label>Email*
       <input name="email" type="email" required /></label>
       <label>Name
