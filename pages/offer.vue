60853381f0bd1ba6df596febe3c592cf86814252
SaftpresseWust <forum@tobiaswust.de>
2018-05-27 16:46:41 +0200

    material offers

--- pages/offer.vue
+++ pages/offer.vue
@@ -2,35 +2,66 @@
   <div>
     <h1>Angebot</h1>
     <hr>
-    <h2>Internetseiten</h2>
-    <p>Ich erstelle für dich eine Internetseite. Dabei wähle ich anhand deiner Anforderungen die für dich beste Technologie aus. Du musst selbst davon keine Ahnung haben aber ich arbeite mit:</p>
-    <ul>
-      <li>HTML5, CSS3, JS</li>
-      <li>Vue.js</li>
-      <li>Wordpress</li>
-    </ul>
-    <p>Danach kann ich mich auch um deine Suchmaschinenoptimierung kümmern.</p>
 
-  <h2>HTML5 Apps</h2>
-  <p>Ich entwickle für dich HTML5 Apps (Progessive Webapps). Diese können online oder offline funktionieren, am Desktop oder auch Mobil. Dafür verwende ich je nach Anforderung:
-  <ul>
-    <li>Vanilla JS</li>
-    <li>Vue.js</li>
-  </ul>
+<div class="products">
+            <div class="product material">
+                <h2>Internetseiten</h2>
+                <img class="productthump" src="~/assets/img/websites.svg" alt="">
+                <p>Ich erstelle für dich eine <strong>Internetseite</strong>. Dabei wähle ich anhand deiner Anforderungen die für dich beste Technologie aus. Du musst selbst davon keine Ahnung haben aber ich arbeite mit:</p>
+                <ul>
+                    <li>
+                        HTML5, CSS3, JS
+                    </li>
+                    <li>
+                        Vue.js
+                    </li>
+                    <li>
+                        Wordpress
+                    </li>
+                </ul>
+                <p>Danach kann ich dich bei deiner <strong>SEO</strong> (Suchmaschinenoptimierung) unterstützen.</p>
+            </div>
 
-  <h2>SAP ABAP</h2>
-  <p>Inhouse SAP-Programmierer lohnen sich nicht für dein Unternehmen und ganze SAP Berater sind zu teuer für eine Aufgabe? Ich bringe einige Jahre Erfahrung in der ABAP Entwicklung mit und zusammen mit dir kann ich spezielle Reports für dein SAP-System erstellen.</p>
-  
-  <h2>Minigames</h2>
-  <p>Du hast eine Idee für ein Spiel, bist Grafiker, der seine gemalten Figuren zum Leben erwecken will oder möchtest ein Spiel mit deinem Firmenlogo? Ich erstelle für dich 2D Spiele für den Browser, Mobil (Android) oder Desktop. Je nach Anforderung nutze ich dafür:</p>
-  <ul>
-    <li>Phaser.io</li>
-    <li>Construct 2</li>
-  </ul>
-  
-  <h2>Außerdem</h2>
-  <p>Ich arbeite auch gerne mit Powershell und mit AutoHotKey. Wahrscheinlich kann man auch bei dir wiederkehrende Prozesse mit diesen Tools zum Teil automatisieren.</p>
-  <p>Ich habe ein großes Netzwerk aus professionellen Grafikdesignern, Linuxnerds und Programmierern jeder Art. Wenn ich dir selbst nicht helfen kann, dann profitierst du von meinen Connections.</p>
+            <div class="product material">
+                <h2>Progressive Webapps</h2>
+                <img class="productthump" src="~/assets/img/webapps.svg" alt="">
+                <p>Ich entwickle für dich HTML5 Apps. Diese können online oder offline funktionieren, am Desktop oder auch Mobil. Dafür verwende ich je nach Anforderung:</p>
+                <ul>
+                    <li>
+                        Vanilla JS (ES6)
+                    </li>
+                    <li>
+                        Vue.js
+                    </li>
+                </ul>
+            </div>
+
+            <div class="product material">
+                <h2>Minigames</h2>
+                <img class="productthump" src="~/assets/img/games.svg" alt="">
+                <p> Du hast eine Idee für ein Spiel, bist Grafiker, der seine gemalten Figuren zum Leben erwecken will oder möchtest ein Spiel mit deinem Firmenlogo? Ich erstelle für dich <strong>2D Spiele</strong> für den Browser, Mobil (Android) oder Desktop.</p>
+                <ul>
+                    <li>
+                        Phaser.io
+                    </li>
+                    <li>
+                        Construct 2
+                    </li>
+                </ul>
+            </div>
+
+            <div class="product material">
+                <h2>SAP ABAP</h2>
+                <img class="productthump" src="~/assets/img/sap.svg" alt="">
+                <p> Inhouse SAP-Programmierer lohnen sich nicht für dein Unternehmen und ganze SAP Berater sind zu teuer für eine Aufgabe? Ich bringe einige Jahre Erfahrung in der ABAP Entwicklung mit und zusammen mit dir kann ich spezielle Reports für dein SAP-System erstellen. </p>
+            </div>
+
+            <div class="product material">
+                <h2> Connections</h2>
+                <p> Ich habe ein großes <strong>Netzwerk</strong> aus professionellen Grafikdesignern, Linuxnerds und Programmierern jeder Art. Wenn ich dir selbst nicht helfen kann, dann profitierst du von meinen Connections.
+                </p>
+            </div>
+        </div>
 
   </div>
 </template>
@@ -40,4 +71,42 @@ export default {};
 </script>
 
 <style scoped>
+.product {
+  min-width: 300px;
+  width: 40%;
+  text-align: center;
+  padding: 0px 30px 30px 30px;
+  margin: 10px;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+ul {
+  padding: 0;
+}
+.product li {
+  list-style: none;
+  font-weight: bold;
+}
+
+.productthump {
+  margin: auto;
+}
+
+.material {
+  -webkit-box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.14),
+    0 2px 7px 0 rgba(0, 0, 0, 0.12), 0 5px 3px -5px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.14), 0 2px 7px 0 rgba(0, 0, 0, 0.12),
+    0 5px 3px -5px rgba(0, 0, 0, 0.2);
+}
+
+.products {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  -ms-flex-pack: distribute;
+  justify-content: space-around;
+}
 </style>
