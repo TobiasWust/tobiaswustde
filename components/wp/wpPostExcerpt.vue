000aa020edde75fcde5520adae92deabd8e6366e
SaftpresseWust <forum@tobiaswust.de>
2018-06-06 16:39:26 +0200

    starting to make the blog sexy

--- components/wp/wpPostExcerpt.vue
+++ components/wp/wpPostExcerpt.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-      <h2>{{ post.title.rendered }}</h2>
-      <nuxt-link :to="post.slug + '/'">{{ post.slug }}</nuxt-link>
+      <h2><nuxt-link :to="post.slug + '/'">{{ post.title.rendered }}</nuxt-link></h2>
+      <p>{{ post.slug }}</p>
       <div v-html="post.excerpt.rendered"></div>
   </div>
 </template>
