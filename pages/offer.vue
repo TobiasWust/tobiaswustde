feeadba4a242ac73a43df3aa3c1e18f17c68ebe7
Tobias Wust <kontakt@tobiaswust.de>
2019-12-05 01:22:43 +0100

    add uses page, update offers

--- pages/offer.vue
+++ pages/offer.vue
@@ -5,65 +5,30 @@
 
 <div class="products">
             <div id="website" class="product material">
-                <h2>Websites</h2>
+                <h2>Wordpress</h2>
                 <img class="productthump" src="~/assets/img/websites.svg" alt="">
-                <p>Ich erstelle für dich eine <strong>Internetseite</strong>. Dabei wähle ich anhand deiner Anforderungen die für dich beste Technologie aus. Du musst selbst davon keine Ahnung haben aber ich arbeite mit:</p>
+                <p>Ich berate Webdesigner mit Wordpress. Dabei kann ich dich von der Ersteinrichtung bis zu individuellen komplexen Plugins unterstützen.</p>
                 <ul>
                     <li>
                         HTML5, CSS3, JS (ES6)
                     </li>
                     <li>
-                        Vue.js
-                    </li>
-                    <li>
                         Wordpress
                     </li>
                 </ul>
-                <p>Danach kann ich dich bei deiner <strong>SEO</strong> (Suchmaschinen&shy;optimierung) unterstützen.</p>
             </div>
 
             <div id="pwa" class="product material">
-                <h2>Progressive Webapps</h2>
+                <h2>Fullstack Entwicklung</h2>
                 <img class="productthump" src="~/assets/img/webapps.svg" alt="">
-                <p>Ich entwickle für dich HTML5 Apps. Diese können online oder offline funktionieren, am Desktop oder auch Mobil. Dafür verwende ich je nach Anforderung:</p>
+                <p>Ich entwickle im MEVN Stack komplexe Apps, Websites oder sogar Spiele.</p>
                 <ul>
-                    <li>
-                        Vanilla JS (ES6)
-                    </li>
-                    <li>
-                        Vue.js
-                    </li>
-                    <li>
-                        Cordova
-                    </li>                    
+                    <li>Mongo</li>
+                    <li>Express</li>
+                    <li>Vue</li>
+                    <li>Node</li>
                 </ul>
             </div>
-
-            <!-- <div class="product material">
-                <h2>Minigames</h2>
-                <img class="productthump" src="~/assets/img/games.svg" alt="">
-                <p> Du hast eine Idee für ein Spiel, bist Grafiker, der seine gemalten Figuren zum Leben erwecken will oder möchtest ein Spiel mit deinem Firmenlogo? Ich erstelle für dich <strong>2D Spiele</strong> für den Browser, Mobil (Android) oder Desktop.</p>
-                <ul>
-                    <li>
-                        Phaser.io
-                    </li>
-                    <li>
-                        Construct 2
-                    </li>
-                </ul>
-            </div> -->
-
-            <!-- <div id="abap" class="product material">
-                <h2>SAP ABAP</h2>
-                <img class="productthump" src="~/assets/img/sap.svg" alt="">
-                <p> Inhouse SAP-Programmierer lohnen sich nicht für dein Unternehmen und ganze SAP Berater sind zu teuer für eine Aufgabe? Ich bringe einige Jahre Erfahrung in der ABAP Entwicklung mit und zusammen mit dir kann ich spezielle Reports für dein SAP-System erstellen. </p>
-            </div> -->
-
-            <!-- <div class="product material">
-                <h2>Außerdem</h2>
-                <p>Ich habe ein großes <strong>Netzwerk</strong> aus professionellen Grafikdesignern, Linuxnerds und Programmierern jeder Art. Wenn ich dir selbst nicht helfen kann, dann profitierst du von meinen Connections.
-                </p>
-            </div> -->
         </div>
 
   </div>
@@ -78,7 +43,7 @@ export default {
         hid: "description",
         name: "description",
         content:
-          "Hi, ich bin Tobias Wust. Ich bin ein reisender Fullstack Entwickler. Ich mache für dich Websites mit Wordpress oder Vue, Progressive Webapps mit Vue oder Entwickle in ABAP für dein SAP System."
+          "Hi, ich bin Tobias Wust. Ich bin ein reisender Fullstack Entwickler. Ich arbeite mit dem MEVN Stack (Mongo, Express, Vue, Node). Und auch mit Wordpress."
       }
     ]
   }
